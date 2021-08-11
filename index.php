0b0b6ea2991ed5db651936e893fededfab2746c7
Yra <48652707+yra77@users.noreply.github.com>
2021-08-11 06:29:27 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   <meta charset="utf-8">
+   <title>Bouncing balls</title>
+   <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+ 
+   <div class="canvDiv">
+      <canvas></canvas>
+   </div>
+
+   <script type="text/javascript" src="game.js"></script>
+</body>
+
+</html>
